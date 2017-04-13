7134866ecb7cc2d6b7ef257971ac372fab99c52d
t-okuaki <t-okuaki@nekojarashi.com>
2017-04-13 14:44:44 +0900

    Rename

--- lib/slackgetter.rb
+++ lib/slackgetter.rb
@@ -10,23 +10,23 @@ module Slackgetter
     end
   end
 
-  def self.create(channel_name, oldest, latest)
-    channel_id = get_channel_id(channel_name)
+  def self.messages(channel_name, oldest, latest)
+    channel_id = channel_id(channel_name)
     oldest = Time.parse(oldest)
     latest = Time.parse(latest)
 
     messages = Slack.channels_history(channel: channel_id, oldest: oldest.to_i, latest: latest.to_i)
-    text = get_text(messages)
+    text = text(messages)
     puts text
   end
 
-  def self.get_channel_id(channel_name)
+  def self.channel_id(channel_name)
     channels = Slack.channels_list["channels"]
     channel = channels.select{|channel| channel["name"] == channel_name}
     channel_id = channel.first["id"]
   end
 
-  def self.get_text(messages)
+  def self.text(messages)
     if messages["ok"] == true
       text = messages["messages"].map{|message| message["text"]}
       convert(text)
