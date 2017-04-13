454423887f83d24be02ed5a156a5d0b810612d69
t-okuaki <t-okuaki@nekojarashi.com>
2017-04-13 14:33:02 +0900

    Add getting message from slack

--- lib/slackgetter.rb
+++ lib/slackgetter.rb
@@ -1,5 +1,49 @@
 require "slackgetter/version"
+require "slack"
+require "time"
 
 module Slackgetter
-  # Your code goes here...
+
+  def self.configure_token(token)
+    Slack.configure do |config|
+      config.token = token
+    end
+  end
+
+  def self.create(channel_name, oldest, latest)
+    channel_id = get_channel_id(channel_name)
+    oldest = Time.parse(oldest)
+    latest = Time.parse(latest)
+
+    messages = Slack.channels_history(channel: channel_id, oldest: oldest.to_i, latest: latest.to_i)
+    text = get_text(messages)
+    puts text
+  end
+
+  def self.get_channel_id(channel_name)
+    channels = Slack.channels_list["channels"]
+    channel = channels.select{|channel| channel["name"] == channel_name}
+    channel_id = channel.first["id"]
+  end
+
+  def self.get_text(messages)
+    if messages["ok"] == true
+      text = messages["messages"].map{|message| message["text"]}
+      convert(text)
+    end
+  end
+
+  def self.convert(text)
+    text.reverse.join("\n\n").gsub(/\&lt;/, '<').gsub(/\&gt;/, '>')
+        .gsub(/<(http[^>]*)>/){ $1 }
+        .gsub(/\*([^*]*)\*/){ "*#{$&}*" }
+        .gsub(/\~([^~]*)\~/){ "~#{$&}~" }
+    # 離れている ~ 同士で ~~ になってしまう
+  end
+
+  def self.convert_link_of(text)
+    # text.gsub(/<(http[^>]*)>/, '\1')
+    text.gsub(/<(http[^>]*)>/){ $1 }
+  end
+
 end
