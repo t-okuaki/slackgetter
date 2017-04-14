0e6d7439e58e0adb3d77fce2dd6558c6cf8f20ce
t-okuaki <t-okuaki@nekojarashi.com>
2017-04-14 14:10:58 +0900

    User thor

--- lib/slackgetter/cli.rb
+++ lib/slackgetter/cli.rb
@@ -1,10 +1,16 @@
 require 'thor'
+require 'slackgetter'
+require 'pry'
 
 module Slackgetter
   class CLI < Thor
-    desc "hello", "an example task"
-    def hello
-      puts "hello!!"
+    desc "time", "get messages"
+    def time(channel_name, oldest=nil, latest=nil)
+      Slackgetter.configure_token ENV['SLACK_TOKEN']
+      oldest ||= (Time.now - 24*60*60).to_s
+      latest ||= Time.now.to_s
+      messages = Slackgetter.messages(channel_name, oldest, latest)
+      puts messages
     end
   end
 end
